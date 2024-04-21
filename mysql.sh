089f49af5e32732a70e73a9bab2a46f724441d9e
madhankumarchavan <madhankumarchavan26@gmail.com>
2024-04-21 17:37:20 +0530

    commit

--- mysql.sh
+++ mysql.sh
@@ -7,39 +7,40 @@ TIMESTAMP=$(date +%F-%H-%M-%S)
 SCRIPT_NAME=$( echo $0 | cut -d"."-f1 )
 LOGFILE=/tmp/$USERID-$TIMESTAMP-$SCRIPT_NAME.log
 
-#function to keep code dry decalaring variables
+#colours declaration
+R="e\[31m"
+G="e\[32m"
+Y="e\[33m"
 
+#function to keep code dry decalaring variables
 VALIDATE () {
 if [ $1 -ne 0]
 then
- echo -e $R"script failure"
+ echo -e "$2.....$R script failure $N"
  exit 1
 else
-echo -e $Y"script passed"
+echo -e "$2......script passed $N"
 fi
 }
 
-#colours declaration
-R="e\31m"
-G="e\32m"
-Y="e\33m
+
 
 if [ $USERID -ne 0 ]
- then
-  echo -e $R"run with root acces"$N
+then
+  echo -e $R"run with root acces"$N 
   exit 1
 else
   echo -e $Y"running with root acces"$N
 fi
 
-dnf install mysql-server -y
+dnf install mysql-server -y &>>$LOGFILE
   VALIDATE $? "insatlling mysql"
 
-systemctl enable mysqld
+systemctl enable mysqld &>>$LOGFILE
  VALIDATE $? "enable mysql"
 
- systemctl start mysqld
+ systemctl start mysqld &>>$LOGFILE
   VALIDATE $? "start mysql"
 
-mysql_secure_installation --set-root-pass ExpenseApp@1
+mysql_secure_installation --set-root-pass ExpenseApp@1 &>>$LOGFILE
  VALIDATE $? "secure mysql"
